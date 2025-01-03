a02b49de418f97e55acbc5c3a414bb1c218828d9
KaiacUPV <kaoican@upv.edu.es>
2025-01-03 02:23:09 +0100

    5.cpp

--- problemas/dia5.cpp
+++ problemas/dia5.cpp
@@ -3,101 +3,129 @@
 #include <string>
 #include <cctype>
 #include <fstream>
+#include <sstream>
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stdlib.h>
+
+
 
 
 using namespace std;
 
-bool in(vector<int> v, int num){   //devuelve si num se encuentra en v
-    for(int i = 0; i < v.size(); i++){
-        if (v[i] == num)
-            return true;
+
+//split que devuelve un vector de enteros en base al delimitador
+vector<int> split(const string& str, char delimiter) {
+    vector<int> result;
+    istringstream ss(str);
+    string token;
+
+    while (getline(ss, token, delimiter)) {
+        // Eliminar espacios en blanco al inicio y al final
+        token.erase(token.begin(), find_if(token.begin(), token.end(), [](unsigned char ch) {
+            return !isspace(ch);
+        }));
+        token.erase(find_if(token.rbegin(), token.rend(), [](unsigned char ch) {
+            return !isspace(ch);
+        }).base(), token.end());
+        
+        try {
+            result.push_back(stoi(token));
+        } catch (const invalid_argument&) {
+            throw runtime_error("Error: La cadena contiene elementos no convertibles a enteros.");
+        }
     }
-    return false;
+
+    return result;
 }
 
-bool va_antes(int a, int b, vector<int> normas[]){  //devuelve si a va antes que b
-    if(in(normas[b-10], a)){
-        return true;
-    }
-    return false;
 
+void imprimirGrafo(const unordered_map<int, unordered_set<int>>& grafo) {
+    for (const auto& par : grafo) {
+        cout << "Clave: " << par.first << " -> ";
+        for (int num : par.second) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
 }
 
-bool cumple_reglas(vector<int> input, vector<int> normas[]){ 
-    for(int i = 0; i < input.size(); i++){
-        for(int j = i + 1 ; j < input.size(); j++){
-            if(va_antes(input[j], input[i], normas))  //si se encuentra después un número que debe ir antes es incorrecta
+
+
+
+//devuelve si b debe de ir después de a según las reglas
+bool a_b(int a, int b, const unordered_map<int, unordered_set<int>>& grafo){
+     return find(grafo.at(a).begin(), grafo.at(a).end(), b) != grafo.at(a).end();
+}
+    
+
+//devuelve si la secuencia cumple con las reglas
+bool cumple_reglas(vector<int> input, const unordered_map<int, unordered_set<int>>& grafo){ 
+    for(int i = input.size() - 1; i >= 0; i--){
+        for(int j = 0 ; j < i; j++){
+            if(a_b(input[i], input[j], grafo))  //si se encuentra una j que debe ir después de i, antes que i, la linea es incorrecta  
                 return false;
         }
     }
     return true;
 }
 
-
-int medio(vector<int> v){ //devuelve el elemento de la mitad
-    int pos = v.size()/2;
-    return v[pos];
+//devuelve el elemento de la mitad
+int medio(vector<int> v){ 
+    return v[v.size()/2];
 }
 
 
-void merge(vector<int>& vec, int left, int mid, int right, vector<int> normas[]) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    vector<int> leftVec(n1), rightVec(n2);
-
-    for (i = 0; i < n1; i++)
-        leftVec[i] = vec[left + i];
-    for (j = 0; j < n2; j++)
-        rightVec[j] = vec[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (va_antes(leftVec[i], rightVec[j], normas)) {  
-            vec[k] = leftVec[i];
-            i++;
-        } else {
-            vec[k] = rightVec[j];
-            j++;
+//orden topológico
+vector<int> sortUpdate(const vector<int> &update, const unordered_map<int, unordered_set<int>>& grafo) {
+    unordered_map<int, int> inDegree;  
+    unordered_map<int, vector<int>> grafo_temp;  
+    
+    // Construimos el grafo temporal y calculamos los grados de entrada
+    for (int nodo : update) {
+        inDegree[nodo] = 0;
+    }
+    for (const auto &regla : grafo) {
+        int A = regla.first;
+        for (int B : regla.second) {
+            if (inDegree.count(A) && inDegree.count(B)) {
+                grafo_temp[A].push_back(B);
+                ++inDegree[B];
+            }
         }
-        k++;
     }
 
-    while (i < n1) {
-        vec[k] = leftVec[i];
-        i++;
-        k++;
+    // Hacemos el orden topológico con el algoritmo de Kahn
+    queue<int> q;
+    for (const auto &entrada : inDegree) {
+        if (entrada.second == 0) {
+            q.push(entrada.first);   //Metemos en la cola todos los nodos con 0 entradas
+        }
     }
 
-    while (j < n2) {
-        vec[k] = rightVec[j];
-        j++;
-        k++;
+    vector<int> ordenado;            
+    while (!q.empty()) {             //procesamos los nodos mientras la cola no esté vacía
+        int actual = q.front(); 
+        q.pop();                     //sale el primero de la cola
+        ordenado.push_back(actual);  //y lo añadimos al vector ordenado
+        for (int vecino : grafo_temp[actual]) {  //decrementamos en 1 el grado de entrada de todos los nodos a los que apuntaba
+            --inDegree[vecino];
+            if (inDegree[vecino] == 0) {  //si se queda sin entradas lo metemos a la cola
+                q.push(vecino);
+            }
+        }
     }
+    return ordenado;
 }
 
-
-void mergeSort(vector<int>& vec, int left, int right, vector<int> normas[]) {
-    if(left < right){
-             
-        int mid = left + (right - left) / 2;
-  
-        mergeSort(vec, left, mid, normas);
-        mergeSort(vec, mid + 1, right,normas);
-
-        merge(vec, left, mid, right,normas);
-    }
-}
 
 
 int main() {
-    vector<int> normas[90]; 
+    unordered_map<int, unordered_set<int>> grafo;
+    unordered_map<int, int> inDegree;
     string linea;
-    
     int suma_correctos = 0;
     int suma_incorrectos = 0;
 
@@ -108,35 +136,38 @@ int main() {
     }
 
     while (getline(archivo, linea)){
-        vector<int> input_line;
         if (linea[2]=='|'){
-            int a = (linea[0]-'0')*10 + (linea[1]-'0');
-            int b = (linea[3]-'0')*10 + (linea[4]-'0');
-            normas[b-10].push_back(a);
+            
+            int A = (linea[0]-'0')*10 + (linea[1]-'0');
+            int B = (linea[3]-'0')*10 + (linea[4]-'0');
+        
+            grafo[A].insert(B);     
         }
+
+
+
         else if (linea[2]==','){
-            size_t pos;
-            while ((pos=linea.find(',')) != string::npos){
-                input_line.push_back(stoi(linea.substr(0,pos)));
-                linea.erase(0,pos+1);
-            }
+            vector<int> update;
 
-            input_line.push_back(stoi(linea));
-
-            if(cumple_reglas(input_line, normas)){
-                suma_correctos+= medio(input_line);
+           update = split(linea, ',');
+            
+            if(cumple_reglas(update, grafo)){
+            
+            suma_correctos += medio(update);
+            
             }else{
-                mergeSort(input_line, 0, input_line.size() - 1, normas);
-                suma_incorrectos += medio(input_line);
+                update = sortUpdate(update, grafo);
+                suma_incorrectos += medio(update);
             }
         }
-     }
+       
+    }
+    archivo.close();    
 
-    archivo.close(); 
 
+    //imprimirGrafo(grafo);
     cout << "Suma de correctos: " << suma_correctos << endl;
-        cout << "Suma de incorrectos ordenados: " << suma_incorrectos<< endl;
-
+    cout << "Suma de incorrectos ordenados: " << suma_incorrectos<< endl;
     return 0;
 }
 
